2906db9ac07201b571b4463e92cf36612cfef881
benson <mwangibenson923@gmail.com>
2024-09-23 13:43:55 +0300

    base

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from './firebase'; 
 import firebase from './firebase'; // or similar
 
 
